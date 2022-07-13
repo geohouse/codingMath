9d6d3d9e8ac8522ccd06b5b6be3ee39d5f78f571
Geoffrey House <geo.house7@gmail.com>
2022-07-13 17:05:55 -0600

    Update arctan.js

--- arctan.js
+++ arctan.js
@@ -9,9 +9,11 @@ window.onload = function () {
     // distance between arrow and the mouse
     dx,
     dy,
-    x,
-    y,
-    angle = 0,
+    x = 0,
+    y = 0,
+    angle = Math.PI / 4,
+    angleTest = 0,
+    //angle = 0,
     animateAngle = 0,
     speed = 0.01,
     radius = 200,
@@ -23,8 +25,8 @@ window.onload = function () {
     context.translate(arrowX, arrowY);
     context.rotate(angle);
     // Canvas already offset by arrowX and arrowY, so don't need any other offset.
-    x = Math.cos(animateAngle) * radius;
-    y = Math.sin(animateAngle) * radius;
+    //x = Math.cos(animateAngle) * radius;
+    //y = Math.sin(animateAngle) * radius;
     context.beginPath();
 
     context.moveTo(x + 20, y + 0);
@@ -39,20 +41,40 @@ window.onload = function () {
     context.fill();
     // For testing animation speeds.
     prevHolder = animateAngle;
-    animateAngle += speed;
+    //animateAngle += speed;
     //console.log(animateAngle - prevHolder);
     context.restore();
     requestAnimationFrame(render);
   }
 
+  // event.clientX and clientY are relative to upper left corner
+  // x and y are relative to the translated 0,0 point at arrowX and arrowY,
+  // and are also on rotated coordinates by angle.
+  let rescaleX = 0,
+    rescaleY = 0,
+    rescaleRotateX = 0,
+    rescaleRotateY = 0;
   document.body.addEventListener("mousemove", function (event) {
-    console.log(event.clientX);
+    rescaleX = event.clientX - width / 2;
+    // -1 needed to flip the y axis
+    rescaleY = -1 * (event.clientY - height / 2);
+    // Now need to rotate by the previous angle (used to render the arrow, and used for the x, y coords)
+    console.log(rescaleX);
     console.log({ x });
-    console.log(event.clientY);
+    console.log(rescaleY);
     console.log({ y });
-    dx = Math.abs(event.clientX - x);
-    dy = Math.abs(event.clientY - y);
-    angle = Math.atan2(dy, dx);
+    // Rotate axes CW
+    rescaleRotateX =
+      -1 * rescaleX * Math.cos(angle) + rescaleY * Math.sin(angle);
+    rescaleRotateY = rescaleX * Math.sin(angle) + rescaleY * Math.cos(angle);
+    console.log({ rescaleRotateX });
+    console.log({ rescaleRotateY });
+
+    dx = rescaleRotateX - x;
+    dy = rescaleRotateY - y;
+    angleTest = 180 - Math.atan2(dy, dx) * (180 / Math.PI);
+    console.log({ angleTest });
+    //angle = Math.atan2(dy, dx);
     console.log({ dx });
     console.log({ dy });
     console.log(angle);
