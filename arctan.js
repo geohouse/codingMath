8eced02f8a0ef779cdf74de2815ac448dedd2d3f
Geoffrey House <geo.house7@gmail.com>
2022-07-12 23:29:33 -0600

    Update arctan.js

--- arctan.js
+++ arctan.js
@@ -12,36 +12,50 @@ window.onload = function () {
     x,
     y,
     angle = 0,
+    animateAngle = 0,
     speed = 0.01,
-    radius = 200;
+    radius = 200,
+    prevHolder = 0;
 
   function render() {
     context.clearRect(0, 0, width, height);
     context.save();
     context.translate(arrowX, arrowY);
     context.rotate(angle);
-
+    // Canvas already offset by arrowX and arrowY, so don't need any other offset.
+    x = Math.cos(animateAngle) * radius;
+    y = Math.sin(animateAngle) * radius;
     context.beginPath();
-    context.moveTo(20, 0);
-    context.lineTo(-20, 0);
-    context.moveTo(20, 0);
-    context.lineTo(10, -10);
-    context.moveTo(20, 0);
-    context.lineTo(10, 10);
+
+    context.moveTo(x + 20, y + 0);
+    context.lineTo(x - 20, y + 0);
+    context.moveTo(x + 20, y + 0);
+    context.lineTo(x + 10, y - 10);
+    context.moveTo(x + 20, y + 0);
+    context.lineTo(x + 10, y + 10);
+    //context.arc(x, y, 10, 0, Math.PI * 2, false);
     context.stroke();
+
+    context.fill();
+    // For testing animation speeds.
+    prevHolder = animateAngle;
+    animateAngle += speed;
+    //console.log(animateAngle - prevHolder);
     context.restore();
     requestAnimationFrame(render);
   }
 
   document.body.addEventListener("mousemove", function (event) {
-    dx = event.clientX - arrowX;
-    dy = event.clientY - arrowY;
+    console.log(event.clientX);
+    console.log({ x });
+    console.log(event.clientY);
+    console.log({ y });
+    dx = Math.abs(event.clientX - x);
+    dy = Math.abs(event.clientY - y);
     angle = Math.atan2(dy, dx);
-    x = centerX + Math.cos(angle) * radius;
-    y = centerY + Math.sin(angle) * radius;
-    context.beginPath();
-    context.arc(x, y, 10, 0, Math.PI * 2, false);
-    context.fill();
+    console.log({ dx });
+    console.log({ dy });
+    console.log(angle);
   });
 
   render();
