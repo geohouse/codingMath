cf3cace9ab2a7b1f0b05554014dff6f4a463b171
Geoffrey House <geo.house7@gmail.com>
2022-07-14 18:08:30 -0600

    update tests

--- vector.js
+++ vector.js
@@ -1,46 +1,51 @@
-export class Vector{
-    #x;
-    #y;
-    constructor(xInput, yInput){
-        this.#x = xInput;
-        this.#y = yInput;
-    }
+export class Vector {
+  // Need to declare private instance variables before setting them in the constructor.
+  #x;
+  #y;
+  constructor(xInput, yInput) {
+    this.#x = xInput;
+    this.#y = yInput;
+  }
 
-    get x(){
-        return this.#x;
-    }
+  get x() {
+    return this.#x;
+  }
 
+  set x(value) {
+    this.#x = value;
+  }
 
-    set x(value){
-        this.#x = value;
-    }
+  get y() {
+    return this.#y;
+  }
 
-    get y(){
-        return this.#y;
-    }
+  set y(value) {
+    this.#y = value;
+  }
 
-    set y(value){
-        this.#y = value;
-    }
+  setAngle(angle) {
+    let length = this.getLength();
+    this.#x = Math.cos(angle) * length;
+    this.#y = Math.sin(angle) * length;
+  }
 
-    setAngle(angle){
-        let length = this.getLength();
-        this.#x = Math.cos(angle) * length;
-        this.#y = Math.sin(angle) * length;
-    }
+  getAngle() {
+    return Math.atan2(this.#y, this.#x);
+  }
 
-    getAngle(){
-        return Math.atan2(this.#y, this.#x);
-    }
+  setLength(length) {
+    let angle = this.getAngle();
+    this.#x = Math.cos(angle) * length;
+    this.#y = Math.sin(angle) * length;
+  }
 
-    setLength(length){
-        let angle = this.getAngle();
-        this.#x = Math.cos(angle) * length;
-        this.#y = Math.sin(angle) * length;
-    }
-
-    getLength(){
-        return Math.sqrt(this.#x * this.#x + this.#y * this.#y);
-    }
+  //getLength() {
+  //  return Math.sqrt(this.#x * this.#x + this.#y * this.#y);
+  //}
+  test() {
+    return "test";
+  }
+  add(v2) {
+    return "test"; //new Vector(this.#x + v2.x, this.#y + v2.y);
+  }
 }
-  
--- vector_main.js
+++ vector_main.js
@@ -1,6 +1,22 @@
-import {Vector} from "./vector.js"
+import { Vector } from "./vector.js";
 
-let v1 = new Vector(10,5);
+let v1 = new Vector(10, 5);
 console.log(v1);
 console.log(v1.x);
 console.log(v1.y);
+
+console.log(v1.getAngle());
+console.log(v1.getLength());
+
+v1.setAngle(Math.PI / 6);
+v1.setLength(100);
+
+console.log(v1.x);
+console.log(v1.y);
+
+let v2 = new Vector(3, 4);
+let v3 = v1.add(v2);
+
+console.log(v3);
+
+console.log("Gussy!!");
