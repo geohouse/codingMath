2e17c9e530c5969b7f03e2a8e2dbf26833337dcb
Geoffrey House <geo.house7@gmail.com>
2022-07-15 20:55:34 -0600

    working accel and single multi toggle

--- acceleration.js
+++ acceleration.js
@@ -1,24 +1,30 @@
 import { Vector, Particle } from "./physicsClasses.js";
 
+// toggle whether using a single point or 'fireworks'
+let singleToggle = true;
+let p;
 window.onload = function () {
   let canvas = document.getElementById("canvas"),
     context = canvas.getContext("2d"),
     width = (canvas.width = window.innerWidth),
     height = (canvas.height = window.innerHeight),
-    particles = [],
+    gravity = [],
     numParticles = 100;
+  if (!singleToggle) {
+    for (let i = 0; i < numParticles; i++) {
+      gravity.push(
+        new Particle(
+          width / 2,
+          height / 2,
+          Math.random() * 4 + 1,
+          Math.random() * Math.PI * 2
+        )
+      );
+    }
+  } else {
+    p = new Particle(100, height, 10, -Math.PI / 2);
+  }
 
-  //   for (let i = 0; i < numParticles; i++) {
-  //     particles.push(
-  //       new Particle(
-  //         width / 2,
-  //         height / 2,
-  //         Math.random() * 4 + 1,
-  //         Math.random() * Math.PI * 2
-  //       )
-  //     );
-  //   }
-  let p = new Particle(100, height, 10, -Math.PI / 2);
   // This script imports the Particle class
   let accel = new Vector(0.1, 0.1);
   //let position = new Vector(500, 500);
@@ -30,14 +36,23 @@ window.onload = function () {
   function update() {
     context.clearRect(0, 0, width, height);
 
-    //    for (let i = 0; i < numParticles; i++) {
-    //      let p = particles[i];
-    p.accelerate(accel);
-    p.update();
-    //position.addTo(velocity);
-    context.beginPath();
-    context.arc(p.position.x, p.position.y, 10, 0, 2 * Math.PI, false);
-    context.fill();
+    if (!singleToggle) {
+      for (let i = 0; i < numParticles; i++) {
+        let p = gravity[i];
+        p.accelerate(accel);
+        p.update();
+        context.beginPath();
+        context.arc(p.position.x, p.position.y, 10, 0, 2 * Math.PI, false);
+        context.fill();
+      }
+    } else {
+      p.accelerate(accel);
+      p.update();
+      //position.addTo(velocity);
+      context.beginPath();
+      context.arc(p.position.x, p.position.y, 10, 0, 2 * Math.PI, false);
+      context.fill();
+    }
     //}
 
     requestAnimationFrame(update);
