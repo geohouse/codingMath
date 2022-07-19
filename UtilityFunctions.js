d0a8ba9bfebefd2cfec75d6f9b8ab79e3cdd5f78
Geoffrey House <geo.house7@gmail.com>
2022-07-19 17:36:12 -0600

    linear interpolation

--- UtilityFunctions.js
+++ UtilityFunctions.js
@@ -5,3 +5,9 @@
 export function normalize(value, min, max) {
   return (value - min) / (max - min);
 }
+
+// Linear interpolation of values in a range regardless of whether the min value of the range is 0 or not.
+// Is also called lerp
+export function linearInterpolate(norm, min, max) {
+  return (max - min) * norm + min;
+}
