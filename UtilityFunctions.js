63b0c9026dcb30bf8e0c1b25bc246fb8e68c3e01
Geoffrey House <geo.house7@gmail.com>
2022-07-20 21:53:00 -0600

    adding map and clamp

--- UtilityFunctions.js
+++ UtilityFunctions.js
@@ -11,3 +11,17 @@ export function normalize(value, min, max) {
 export function linearInterpolate(norm, min, max) {
   return (max - min) * norm + min;
 }
+
+// Combines the normalize and linearInterpolation functions
+// in order to map a value from one range into another
+export function map(value, sourceMin, sourceMax, destMin, destMax){
+  let n = norm(value, sourceMin, sourceMax);
+  return linearInterpolate(n, destMin, destMax);
+}
+
+// Clamp an input value to guarantee it is within a specified range
+export function clamp(value, min, max){
+  // make sure the value is no less than min
+  let value = Math.max(value, min);
+  return Math.min(value, max);
+}
